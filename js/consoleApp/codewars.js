fceedb28dfe170e0ef3a0eb6f81ddb782c172a43
flutter <learn.flutter@gmail.com>
2022-08-07 19:35:18 +0600

    solved 3rd homework with functions

---
+++ js/consoleApp/codewars.js
@@ -0,0 +1,26 @@
+String.prototype.toAlternatingCase = function () {
+    // Define your method here :)
+    for (let i = 1; i == String.length; i++) {
+      console.log(String);
+      String.prototype.toAlternatingCase;
+        
+    }
+  };
+
+
+
+  const stroka = 'hello world';
+
+  console.log(stroka.slice(-6, -3));
+
+  console.log(stroka.substring(0, 11));
+
+  console.log(stroka.substr(6, 5));
+
+  const num = '12.34asdads';
+
+  console.log(parseInt(num));
+ // console.log(Math.random);
+  console.log(parseFloat(num));
+
+
